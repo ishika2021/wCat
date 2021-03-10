acee41e282ca13ef54b8936adfc41b62f552bf5a
Ishika Mishra <mishraishika7@gmail.com>
2021-03-10 12:24:00 +0530

    modified the view file

--- activity/view.js
+++ activity/view.js
@@ -1,33 +1,22 @@
 let fs=require("fs");
-let path=require("path");
 function isFile(dirpath){
     try {
         let res=fs.lstatSync(dirpath);
         return res.isFile();
     } catch (error) {
-        return console.log("No such file exists");
+        return false
     }
 }
 function viewFileContent(dirpath){
-    //console.log(path.basename(dirpath)+"is a file");
     let data=fs.readFileSync(dirpath,{encoding:'utf8', flag:'r'});
     console.log(data);
-    
-}
-function listContentOfDir(dirpath){
-    return fs.readdirSync(dirpath);
 }
 function view(dirpath){
     let res=isFile(dirpath);
     if(res==true){
         viewFileContent(dirpath);
-    }else if(res==false){
-        let content=listContentOfDir(dirpath);
-        //console.log(content);
-        for(let i=0;i<content.length;i++){
-            let child=path.join(dirpath,content[i]);
-            view(child);
-        }
+    }else{
+        console.log("File doesn't exists");
     }
 }
 
